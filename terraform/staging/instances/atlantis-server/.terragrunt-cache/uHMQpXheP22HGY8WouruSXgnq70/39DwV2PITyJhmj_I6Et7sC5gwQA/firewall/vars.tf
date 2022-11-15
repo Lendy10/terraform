e3c3e613de75388f4d7184cda3e672ab0c47993a
Lendy10 <lendy.pradhana.hartono-2016@fst.unair.ac.id>
2022-11-15 12:10:49 +0700

    checko perez

---
+++ terraform/staging/instances/atlantis-server/.terragrunt-cache/uHMQpXheP22HGY8WouruSXgnq70/39DwV2PITyJhmj_I6Et7sC5gwQA/firewall/vars.tf
@@ -0,0 +1,35 @@
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "credentials" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "network" {
+  type = string
+}
+
+variable "ports" {
+  type = list
+}
+
+variable "source_ranges" {
+  type = list
+}
+
+variable "target_tags" {
+  type = list
+}
