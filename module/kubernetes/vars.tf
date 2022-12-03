ba751d412d4eaada68b2ad860ea8f1e55e8ac2cb
Lendy10 <lendy.pradhana.hartono-2016@fst.unair.ac.id>
2022-12-03 12:38:48 +0700

    Salah

--- module/kubernetes/vars.tf
+++ module/kubernetes/vars.tf
@@ -1,3 +1,19 @@
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "credentials" {
+  type = string
+}
+
 variable "name" {
   type = string
 }
